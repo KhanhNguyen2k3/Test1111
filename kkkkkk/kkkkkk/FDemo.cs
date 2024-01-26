f23f5db047614db4b972f66ed6ba447280588282
nhatkhanh <nguyennhatkhanh12102019@gmail.com>
2024-01-26 15:10:46 +0700

    Them FDemo.cs

---
+++ kkkkkk/kkkkkk/FDemo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kkkkkk
+{
+    public partial class FDemo : Form
+    {
+        public FDemo()
+        {
+            InitializeComponent();
+        }
+    }
+}
